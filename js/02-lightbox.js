f706c22805f051356fa729abe6f810b0d4eba9be
tarabaria <128685314+tarabaria@users.noreply.github.com>
2023-08-21 22:11:13 +0300

    task 2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,29 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
 
-console.log(galleryItems);
+const gallery = document.querySelector('.gallery');
+
+const galleryItemsHTML = galleryItems
+  .map(
+    item => `<li class="gallery__item">
+  <a class="gallery__link" href="${item.original}">
+    <img class="gallery__image" src="${item.preview}" alt="${item.description}" />
+  </a>
+</li>`
+  )
+  .join('');
+
+gallery.insertAdjacentHTML('beforeend', galleryItemsHTML);
+
+// Ініціалізація SimpleLightbox
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
+
+lightbox.on('show.simplelightbox', function () {
+  lightbox.items.captionEl.style.display = 'block';
+});
+
+lightbox.on('close.simplelightbox', function () {
+  lightbox.items.captionEl.style.display = 'none';
+});
